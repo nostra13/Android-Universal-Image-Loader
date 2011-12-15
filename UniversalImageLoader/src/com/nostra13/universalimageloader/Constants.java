839f48057acaf00a0936e44a6d9fe5dace9eb248
Sergey Tarasevich <nostra13@gmail.com>
2011-12-15 21:01:37 +0300

    Added Java docs. Clean code

---
+++ UniversalImageLoader/src/com/nostra13/universalimageloader/Constants.java
@@ -0,0 +1,24 @@
+package com.nostra13.universalimageloader;
+
+/**
+ * Constants
+ * 
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ */
+public class Constants {
+	// Default values of ImageLoaderConfiguration
+	/** {@value} pixels*/
+	public static final int DEFAULT_MAX_IMAGE_WIDTH = 800;
+	/** {@value} pixels */
+	public static final int DEFAULT_MAX_IMAGE_HEIGHT = 480;
+	/** {@value} milliseconds*/
+	public static final int DEFAULT_HTTP_CONNECTION_TIMEOUT = 5000;
+	/** {@value} milliseconds*/
+	public static final int DEFAULT_HTTP_READ_TIMEOUT = 20000;
+	/** {@value} */
+	public static final int DEFAULT_THREAD_POOL_SIZE = 5;
+	/** {@value} bytes */
+	public static final int DEFAULT_MEMORY_CACHE_SIZE = 2000000;
+	/** {@value} */
+	public static final String DEFAULT_CACHE_DIRECTORY = "UniversalImageLoader/Cache";
+}
--- UniversalImageLoader/src/com/nostra13/universalimageloader/imageloader/ImageLoaderConfiguration.java
+++ UniversalImageLoader/src/com/nostra13/universalimageloader/imageloader/ImageLoaderConfiguration.java
@@ -5,6 +5,7 @@ import java.io.File;
 import android.content.Context;
 import android.graphics.Bitmap;
 
+import com.nostra13.universalimageloader.Constants;
 import com.nostra13.universalimageloader.cache.disc.DiscCache;
 import com.nostra13.universalimageloader.cache.disc.impl.DefaultDiscCache;
 import com.nostra13.universalimageloader.cache.memory.MemoryCache;
@@ -13,18 +14,13 @@ import com.nostra13.universalimageloader.utils.StorageUtils;
 
 public final class ImageLoaderConfiguration {
 
-	// Default values
-	private static final String APP_CACHE_DIRECTORY = "UniversalImageLoader/Cache";
-	private static final int MEMORY_CACHE_SIZE = 2000000; // bytes
-
-	// Configuration fields
 	final int maxImageWidthForMemoryCache;
 	final int maxImageHeightForMemoryCache;
 	final int httpConnectTimeout;
 	final int httpReadTimeout;
 	final int threadPoolSize;
-	final DiscCache discCache;
 	final MemoryCache<String, Bitmap> memoryCache;
+	final DiscCache discCache;
 	final DisplayImageOptions defaultDisplayImageOptions;
 
 	private ImageLoaderConfiguration(Builder builder) {
@@ -46,59 +42,95 @@ public final class ImageLoaderConfiguration {
 
 		private Context context;
 
-		private int maxImageWidthForMemoryCache = 800;
-		private int maxImageHeightForMemoryCache = 480;
-		private int httpConnectTimeout = 5000; // milliseconds
-		private int httpReadTimeout = 20000; // milliseconds
-		private int threadPoolSize = 5;
-		private DiscCache discCache = null;
+		private int maxImageWidthForMemoryCache = Constants.DEFAULT_MAX_IMAGE_WIDTH;
+		private int maxImageHeightForMemoryCache = Constants.DEFAULT_MAX_IMAGE_HEIGHT;
+		private int httpConnectTimeout = Constants.DEFAULT_HTTP_CONNECTION_TIMEOUT;
+		private int httpReadTimeout = Constants.DEFAULT_HTTP_READ_TIMEOUT;
+		private int threadPoolSize = Constants.DEFAULT_THREAD_POOL_SIZE;
 		private MemoryCache<String, Bitmap> memoryCache = null;
+		private DiscCache discCache = null;
 		private DisplayImageOptions defaultDisplayImageOptions = null;
 
 		public Builder(Context context) {
 			this.context = context;
 		}
 
+		/**
+		 * Sets maximum image width which will be used for memory saving during decoding an image to {@link Bitmap}.<br />
+		 * Default value - {@link Constants#DEFAULT_MAX_IMAGE_WIDTH}
+		 * */
 		public Builder maxImageWidthForMemoryCache(int maxImageWidthForMemoryCache) {
 			this.maxImageWidthForMemoryCache = maxImageWidthForMemoryCache;
 			return this;
 		}
 
+		/**
+		 * Sets maximum image height which will be used for memory saving during decoding an image to {@link Bitmap}.<br />
+		 * Default value - {@link Constants#DEFAULT_MAX_IMAGE_HEIGHT}
+		 * */
 		public Builder maxImageHeightForMemoryCache(int maxImageHeightForMemoryCache) {
 			this.maxImageHeightForMemoryCache = maxImageHeightForMemoryCache;
 			return this;
 		}
 
+		/**
+		 * Sets timeout for HTTP connection establishment (during image loading).<br />
+		 * Default value - {@link Constants#DEFAULT_HTTP_CONNECTION_TIMEOUT}
+		 * */
 		public Builder httpConnectTimeout(int timeout) {
 			httpConnectTimeout = timeout;
 			return this;
 		}
 
+		/**
+		 * Sets timeout for HTTP reading (during image loading).<br />
+		 * Default value - {@link Constants#DEFAULT_HTTP_READ_TIMEOUT}
+		 * */
 		public Builder httpReadTimeout(int timeout) {
 			httpReadTimeout = timeout;
 			return this;
 		}
 
+		/**
+		 * Sets thread pool size for image display tasks.<br />
+		 * Default value - {@link Constants#DEFAULT_THREAD_POOL_SIZE}
+		 * */
 		public Builder threadPoolSize(int threadPoolSize) {
 			this.threadPoolSize = threadPoolSize;
 			return this;
 		}
 
-		public Builder discCache(DiscCache discCache) {
-			this.discCache = discCache;
-			return this;
-		}
-
+		/**
+		 * Sets memory cache for {@link Bitmap bitmaps}.<br />
+		 * Default value - {@link UsingFreqLimitedCache} with limited memory cache size (
+		 * {@link Constants#DEFAULT_MEMORY_CACHE_SIZE} bytes)
+		 */
 		public Builder memoryCache(MemoryCache<String, Bitmap> memoryCache) {
 			this.memoryCache = memoryCache;
 			return this;
 		}
 
+		/**
+		 * Sets memory cache for {@link Bitmap bitmaps}.<br />
+		 * Default value - {@link DefaultDiscCache}
+		 */
+		public Builder discCache(DiscCache discCache) {
+			this.discCache = discCache;
+			return this;
+		}
+
+		/**
+		 * Sets default {@linkplain DisplayImageOptions display image options} for image displaying. It will be used for
+		 * every {@linkplain ImageLoader#displayImage(String, android.widget.ImageView) image display call} without
+		 * defined custom {@linkplain DisplayImageOptions options}<br />
+		 * Default value - {@link DisplayImageOptions#createSimple() Simple options}
+		 */
 		public Builder defaultDisplayImageOptions(DisplayImageOptions defaultDisplayImageOptions) {
 			this.defaultDisplayImageOptions = defaultDisplayImageOptions;
 			return this;
 		}
 
+		/** Builds configured {@link ImageLoaderConfiguration} object */
 		public ImageLoaderConfiguration build() {
 			initEmptyFiledsWithDefaultValues();
 			context = null;
@@ -107,11 +139,11 @@ public final class ImageLoaderConfiguration {
 
 		private void initEmptyFiledsWithDefaultValues() {
 			if (discCache == null) {
-				File cacheDir = StorageUtils.getCacheDirectory(context, APP_CACHE_DIRECTORY);
+				File cacheDir = StorageUtils.getCacheDirectory(context, Constants.DEFAULT_CACHE_DIRECTORY);
 				discCache = new DefaultDiscCache(cacheDir);
 			}
 			if (memoryCache == null) {
-				memoryCache = new UsingFreqLimitedCache(MEMORY_CACHE_SIZE);
+				memoryCache = new UsingFreqLimitedCache(Constants.DEFAULT_MEMORY_CACHE_SIZE);
 			}
 			if (defaultDisplayImageOptions == null) {
 				defaultDisplayImageOptions = DisplayImageOptions.createSimple();
