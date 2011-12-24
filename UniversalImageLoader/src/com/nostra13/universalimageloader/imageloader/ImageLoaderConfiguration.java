c2996edd585ce8e4806cb195a3cdcbdd4734e178
Sergey Tarasevich <nostra13@gmail.com>
2011-12-24 16:09:56 +0300

    Cache full size images in memory by default

--- UniversalImageLoader/src/com/nostra13/universalimageloader/imageloader/ImageLoaderConfiguration.java
+++ UniversalImageLoader/src/com/nostra13/universalimageloader/imageloader/ImageLoaderConfiguration.java
@@ -68,17 +68,17 @@ public final class ImageLoaderConfiguration {
 	 */
 	public static class Builder {
 
-		/** pixels */
-		public static final int DEFAULT_MAX_IMAGE_WIDTH = 800;
-		/** pixels */
-		public static final int DEFAULT_MAX_IMAGE_HEIGHT = 480;
-		/** milliseconds */
+		/** {@value} pixels */
+		public static final int DEFAULT_MAX_IMAGE_WIDTH = Integer.MAX_VALUE;
+		/** {@value} pixels */
+		public static final int DEFAULT_MAX_IMAGE_HEIGHT = Integer.MAX_VALUE;
+		/** {@value} milliseconds */
 		public static final int DEFAULT_HTTP_CONNECTION_TIMEOUT = 5000;
-		/** milliseconds */
+		/** {@value} milliseconds */
 		public static final int DEFAULT_HTTP_READ_TIMEOUT = 20000;
 		/** {@value} */
 		public static final int DEFAULT_THREAD_POOL_SIZE = 5;
-		/** bytes */
+		/** {@value} bytes */
 		public static final int DEFAULT_MEMORY_CACHE_SIZE = 2000000;
 		/** {@value} */
 		public static final String DEFAULT_CACHE_DIRECTORY = "UniversalImageLoader/Cache";
