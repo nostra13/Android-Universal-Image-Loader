db5fee3856be6cdcf971c02b7d050e1952bab30e
nostra13 <nostra13@gmail.com>
2013-03-09 22:48:29 +0300

    Fixed visibility of methods ImageLoader.denyNetworkDownloads(...) and ImageLoader.handleSlowNetworks(...)

--- library/src/com/nostra13/universalimageloader/core/ImageLoader.java
+++ library/src/com/nostra13/universalimageloader/core/ImageLoader.java
@@ -416,7 +416,7 @@ public class ImageLoader {
 	 * @param denyNetworkDownloads pass <b>true</b> - to deny engine to download images from the network; <b>false</b> -
 	 *            to allow engine to download images from network.
 	 */
-	void denyNetworkDownloads(boolean denyNetworkDownloads) {
+	public void denyNetworkDownloads(boolean denyNetworkDownloads) {
 		engine.denyNetworkDownloads(denyNetworkDownloads);
 	}
 
@@ -427,7 +427,7 @@ public class ImageLoader {
 	 * @param handleSlowNetwork pass <b>true</b> - to use {@link FlushedInputStream} for network downloads; <b>false</b>
 	 *            - otherwise.
 	 */
-	void handleSlowNetwork(boolean handleSlowNetwork) {
+	public void handleSlowNetwork(boolean handleSlowNetwork) {
 		engine.handleSlowNetwork(handleSlowNetwork);
 	}
 
