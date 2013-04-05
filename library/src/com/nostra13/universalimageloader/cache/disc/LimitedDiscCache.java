f641933a6ca867223017abaca7bcd1758a246963
nostra13 <nostra13@gmail.com>
2013-04-06 00:54:34 +0300

    Issue #234 - NPE in LimitedDiscCache

--- library/src/com/nostra13/universalimageloader/cache/disc/LimitedDiscCache.java
+++ library/src/com/nostra13/universalimageloader/cache/disc/LimitedDiscCache.java
@@ -73,11 +73,13 @@ public abstract class LimitedDiscCache extends BaseDiscCache {
 			public void run() {
 				int size = 0;
 				File[] cachedFiles = cacheDir.listFiles();
-				for (File cachedFile : cachedFiles) {
-					size += getSize(cachedFile);
-					lastUsageDates.put(cachedFile, cachedFile.lastModified());
+				if (cachedFiles != null) { // rarely but it can happen, don't know why
+					for (File cachedFile : cachedFiles) {
+						size += getSize(cachedFile);
+						lastUsageDates.put(cachedFile, cachedFile.lastModified());
+					}
+					cacheSize.set(size);
 				}
-				cacheSize.set(size);
 			}
 		}).start();
 	}
