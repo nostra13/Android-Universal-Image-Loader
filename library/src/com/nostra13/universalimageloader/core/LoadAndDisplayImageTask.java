a6d6695a781310e76c9f681e4d3ca86c890af517
nostra13 <nostra13@gmail.com>
2013-04-13 20:35:55 +0300

    Issue #247 - Prevent image decoding if image is reused

--- library/src/com/nostra13/universalimageloader/core/LoadAndDisplayImageTask.java
+++ library/src/com/nostra13/universalimageloader/core/LoadAndDisplayImageTask.java
@@ -220,9 +220,8 @@ final class LoadAndDisplayImageTask implements Runnable {
 					listener.onLoadingCancelled(uri, imageView);
 				}
 			});
+			log(LOG_TASK_CANCELLED);
 		}
-
-		if (imageViewWasReused) log(LOG_TASK_CANCELLED);
 		return imageViewWasReused;
 	}
 
@@ -247,9 +246,11 @@ final class LoadAndDisplayImageTask implements Runnable {
 				log(LOG_LOAD_IMAGE_FROM_NETWORK);
 
 				String imageUriForDecoding = options.isCacheOnDisc() ? tryCacheImageOnDisc(imageFile) : uri;
-				bitmap = decodeImage(imageUriForDecoding);
-				if (bitmap == null) {
-					fireImageLoadingFailedEvent(FailType.DECODING_ERROR, null);
+				if (!checkTaskIsNotActual()) {
+					bitmap = decodeImage(imageUriForDecoding);
+					if (bitmap == null) {
+						fireImageLoadingFailedEvent(FailType.DECODING_ERROR, null);
+					}
 				}
 			}
 		} catch (IllegalStateException e) {
