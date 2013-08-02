e7a3db42331057c011a1f5aa139a6a3012d11f88
taoliang <taoliang1985@gmail.com>
2013-08-02 16:35:33 +0800

    Edit
    Add support of download image which is cached by CDN.

--- library/src/com/nostra13/universalimageloader/core/download/BaseImageDownloader.java
+++ library/src/com/nostra13/universalimageloader/core/download/BaseImageDownloader.java
@@ -203,6 +203,22 @@ public class BaseImageDownloader implements ImageDownloader {
 	 * @throws UnsupportedOperationException if image URI has unsupported scheme(protocol)
 	 */
 	protected InputStream getStreamFromOtherSource(String imageUri, Object extra) throws IOException {
-		throw new UnsupportedOperationException(String.format(ERROR_UNSUPPORTED_SCHEME, imageUri));
+		URL url = new URL("http://" + imageUri);
+		
+		HttpURLConnection http = (HttpURLConnection) url.openConnection();
+		http.setConnectTimeout(5 * 1000);
+		http.setReadTimeout(5 * 1000);
+		http.setRequestMethod("GET");
+		http.setRequestProperty(
+				"Accept",
+				"image/gif, image/jpeg, image/pjpeg, image/pjpeg, application/x-shockwave-flash, application/xaml+xml, application/vnd.ms-xpsdocument, application/x-ms-xbap, application/x-ms-application, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*");
+		http.setRequestProperty("Accept-Language", "zh-CN");
+		http.setRequestProperty("Referer", url.toString());
+		http.setRequestProperty("Charset", "UTF-8");
+		http.setRequestProperty("User-Agent",
+				"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)");
+		http.setRequestProperty("Connection", "Keep-Alive");
+
+		return http.getInputStream();
 	}
 }
