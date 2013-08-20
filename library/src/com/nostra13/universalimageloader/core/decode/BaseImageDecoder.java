c2be1ef7cfb01167de8ca3ec9a2d76c2782a93cd
nostra13 <nostra13@gmail.com>
2013-08-21 02:12:16 +0300

    Clean code

--- library/src/com/nostra13/universalimageloader/core/decode/BaseImageDecoder.java
+++ library/src/com/nostra13/universalimageloader/core/decode/BaseImageDecoder.java
@@ -15,22 +15,21 @@
  *******************************************************************************/
 package com.nostra13.universalimageloader.core.decode;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.os.Build;
-
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.nostra13.universalimageloader.core.download.ImageDownloader.Scheme;
 import com.nostra13.universalimageloader.utils.ImageSizeUtils;
 import com.nostra13.universalimageloader.utils.IoUtils;
 import com.nostra13.universalimageloader.utils.L;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Decodes images to {@link Bitmap}, scales them to needed size
@@ -69,7 +68,6 @@ public class BaseImageDecoder implements ImageDecoder {
 		InputStream imageStream = getImageStream(decodingInfo);
 		ImageFileInfo imageInfo = defineImageSizeAndRotation(imageStream, decodingInfo.getImageUri());
 		Options decodingOptions = prepareDecodingOptions(imageInfo.imageSize, decodingInfo);
-		
 		Bitmap decodedBitmap = decodeStream(imageStream, decodingOptions);
 		if (decodedBitmap == null) {
 			L.e(ERROR_CANT_DECODE_IMAGE, decodingInfo.getImageKey());
@@ -87,6 +85,7 @@ public class BaseImageDecoder implements ImageDecoder {
 		Options options = new Options();
 		options.inJustDecodeBounds = true;
 		BitmapFactory.decodeStream(imageStream, null, options);
+		imageStream.reset();
 
 		ExifInfo exif;
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
@@ -94,8 +93,6 @@ public class BaseImageDecoder implements ImageDecoder {
 		} else {
 			exif = new ExifInfo();
 		}
-		
-		imageStream.reset();
 		return new ImageFileInfo(new ImageSize(options.outWidth, options.outHeight, exif.rotation), exif);
 	}
 
