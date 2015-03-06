cb7462ddaa2e25d7efa663aaef2c7e744b2afb8a
nostra13 <nostra13@gmail.com>
2015-03-07 01:04:45 +0300

    Sample: Clean code

--- sample/src/com/nostra13/universalimageloader/sample/UILApplication.java
+++ sample/src/com/nostra13/universalimageloader/sample/UILApplication.java
@@ -48,15 +48,15 @@ public class UILApplication extends Application {
 		// or you can create default configuration by
 		//  ImageLoaderConfiguration.createDefault(this);
 		// method.
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
-				.threadPriority(Thread.NORM_PRIORITY - 2)
-				.denyCacheImageMultipleSizesInMemory()
-				.diskCacheFileNameGenerator(new Md5FileNameGenerator())
-				.diskCacheSize(50 * 1024 * 1024) // 50 Mb
-				.tasksProcessingOrder(QueueProcessingType.LIFO)
-				.writeDebugLogs() // Remove for release app
-				.build();
+		ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+		config.threadPriority(Thread.NORM_PRIORITY - 2);
+		config.denyCacheImageMultipleSizesInMemory();
+		config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+		config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+		config.tasksProcessingOrder(QueueProcessingType.LIFO);
+		config.writeDebugLogs(); // Remove for release app
+
 		// Initialize ImageLoader with configuration.
-		ImageLoader.getInstance().init(config);
+		ImageLoader.getInstance().init(config.build());
 	}
 }
