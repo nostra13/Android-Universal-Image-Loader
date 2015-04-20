de9d1edd44a787da83e2197a6279ad1165bda741
Vivek Kiran <vivek.kiran@outlook.com>
2015-04-20 19:42:34 +0530

    Updated pom.xml
    Workaround for Robolectric Application Tag Bug
    
    fixed build issue
    
    fixed robolectric test issue
    
    fixed robolectric test issue
    
    Robolectric API Level Issues

--- library/test/com/nostra13/universalimageloader/core/assist/ImageSizeTest.java
+++ library/test/com/nostra13/universalimageloader/core/assist/ImageSizeTest.java
@@ -14,7 +14,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+@Config(emulateSdk = 18) 
 @RunWith(RobolectricTestRunner.class)
 public class ImageSizeTest {
 	private Activity mActivity;
