f6a9615868482672c3630cb7db6dcf43391e80de
nostra13 <nostra13@gmail.com>
2020-04-06 14:30:18 +0300

    [#1343] Fix "No field mMaxHeight" on Android 10. minSdkVersion 14 -> 16.

--- library/src/main/java/com/nostra13/universalimageloader/core/imageaware/ImageViewAware.java
+++ library/src/main/java/com/nostra13/universalimageloader/core/imageaware/ImageViewAware.java
@@ -76,7 +76,7 @@ public class ImageViewAware extends ViewAware {
 		if (width <= 0) {
 			ImageView imageView = (ImageView) viewRef.get();
 			if (imageView != null) {
-				width = getImageViewFieldValue(imageView, "mMaxWidth"); // Check maxWidth parameter
+				width = imageView.getMaxWidth();
 			}
 		}
 		return width;
@@ -93,7 +93,7 @@ public class ImageViewAware extends ViewAware {
 		if (height <= 0) {
 			ImageView imageView = (ImageView) viewRef.get();
 			if (imageView != null) {
-				height = getImageViewFieldValue(imageView, "mMaxHeight"); // Check maxHeight parameter
+				height = imageView.getMaxHeight();
 			}
 		}
 		return height;
@@ -124,20 +124,5 @@ public class ImageViewAware extends ViewAware {
 	@Override
 	protected void setImageBitmapInto(Bitmap bitmap, View view) {
 		((ImageView) view).setImageBitmap(bitmap);
-	}
-
-	private static int getImageViewFieldValue(Object object, String fieldName) {
-		int value = 0;
-		try {
-			Field field = ImageView.class.getDeclaredField(fieldName);
-			field.setAccessible(true);
-			int fieldValue = (Integer) field.get(object);
-			if (fieldValue > 0 && fieldValue < Integer.MAX_VALUE) {
-				value = fieldValue;
-			}
-		} catch (Exception e) {
-			L.e(e);
-		}
-		return value;
 	}
 }
